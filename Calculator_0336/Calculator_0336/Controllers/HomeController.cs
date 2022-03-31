be914118b45d64d4525094327fd57459f3d80fbe
soon-404 <tfpsoon@gmail.com>
2022-03-31 18:33:45 +0700

    move to index

--- Calculator_0336/Calculator_0336/Controllers/HomeController.cs
+++ Calculator_0336/Calculator_0336/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace Calculator_0336.Controllers
         public IActionResult Calculate(string number)
         {
             CalculatorModel cal = new CalculatorModel();
-            string value = new DataTable().Compute(number, null).ToString(); ;
+            string value = new DataTable().Compute(number, null).ToString();
             cal.CalculateValue = value;
             return Content(cal.CalculateValue, "text/html", Encoding.UTF8); 
         }
