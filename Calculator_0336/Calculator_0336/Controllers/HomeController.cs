0747f4b5f1179deb93a5fcbacb3dddf2eebe5cef
soon-404 <tfpsoon@gmail.com>
2022-03-31 13:06:39 +0700

    calculator_finish

---
+++ Calculator_0336/Calculator_0336/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Text;
+using System.Data;
+using Calculator_0336.Models;
+
+namespace Calculator_0336.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Produces("text/html")]
+        public IActionResult Calculate(string number)
+        {
+            CalculatorModel cal = new CalculatorModel();
+            string value = new DataTable().Compute(number, null).ToString(); ;
+            cal.CalculateValue = value;
+            return Content(cal.CalculateValue, "text/html", Encoding.UTF8); 
+        }
+    }
+}
---
+++ Calculator_0336/Calculator_0336/Models/CalculatorModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calculator_0336.Models
+{
+    public class CalculatorModel
+    {
+        public string CalculateValue { get; set; }
+    }
+}
